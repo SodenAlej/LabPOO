383acce5f0e0bd2f55b481481d72a822ec57454a
SodenAlej <118505676+SodenAlej@users.noreply.github.com>
2022-11-19 02:29:54 -0600

    Libreria Practica 10

---
+++ Practica10Lib/src/Lib/Calculadora.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lib;
+
+/**
+ *
+ * @author Usuario
+ */
+public abstract class Calculadora {
+    
+    public abstract double suma(double a, double b);
+    
+    public abstract double resta(double a, double b);
+    
+    public abstract double div(double a, double b);
+    
+    public abstract double multi(double a, double b);
+}
