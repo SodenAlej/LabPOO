4b4549db0720bdbf3291ca2f2a3364283d2bb2c3
SodenAlej <118505676+SodenAlej@users.noreply.github.com>
2022-11-19 01:28:37 -0600

    Add files via upload

---
+++ RunPractica2/RunCalculadora/src/runcalculadora/Operaciones.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package runcalculadora;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Operaciones {
+    public double suma(double a, double b){
+        return a+b;
+    }
+    
+    public double resta(double a, double b){
+        return a-b;
+    }
+    
+    public double div(double a, double b){
+        return a/b;
+    }
+    
+    public double multi(double a, double b){
+        return a*b;
+    }
+    
+}
